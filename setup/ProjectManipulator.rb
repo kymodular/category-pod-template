873f569227df3dcaa91fbadecefd98dc79c8272a
王英辉 <iyinghui@163.com>
2019-06-17 19:36:42 +0800

    update file

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -101,7 +101,7 @@ RUBY
         before = classes_folder + "/" +file
         next unless File.exists? before
 
-        module_name = @configurator.pod_name.gsub("_Category")
+        module_name = @configurator.pod_name.gsub("_Category", "")
         after = classes_folder + "/" + file.gsub("PROJECT", module_name)
         File.rename before, after
       end 
